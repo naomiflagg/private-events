e294c31b32ba89ba81c9faef340d1e429a621eb0
Naomi Flagg <neffly511@gmail.com>
2020-10-13 13:18:18 -0400

    Create scopes for past and upcoming events for views

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :invitations, foreign_key: :invited_event_id
   has_many :invitees, through: :invitations, source: :event_invitee
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
